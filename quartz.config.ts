8236116f2370d89b85da3381e50e3b311baadad2
Alex <alexandros.stars@gmail.com>
2025-11-17 23:25:19 +0300

    init: add cosmetics

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "~ / Alex / CTO",
+    pageTitle: "~/Alex/...",
     pageTitleSuffix: "suffix",
     enableSPA: true,
     enablePopovers: true,
